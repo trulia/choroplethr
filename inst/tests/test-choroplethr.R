9974db5142df4c63f10008d89823445044660de8
Ari Lamstein <arilamstein@gmail.com>
2013-12-22 10:59:59 -0800

    tests.

--- inst/tests/test-choroplethr.R
+++ inst/tests/test-choroplethr.R
@@ -1,25 +1,21 @@
 context("choroplethr")
 
-# rendering a test state-level map at the continuous and discrete scales needs to return a valid ggplot object
 test_that("state level choropleths return ggplots with discrete and continuous lods", {
   df = data.frame(region=state.abb, value=sample(100, 50))
-  expect_that(class(choroplethr(df, lod="state", num_buckets=1)), matches(c("gg", "ggplot")))
-  expect_that(class(choroplethr(df, lod="state", num_buckets=9)), matches(c("gg", "ggplot")))
+  expect_equal(class(choroplethr(df, lod="state", num_buckets=1)), c("gg", "ggplot"))
+  expect_equal(class(choroplethr(df, lod="state", num_buckets=9)), c("gg", "ggplot"))
 })
                         
-# rendering a test county-level map at the continuous and discrete scales needs to return a valid ggplot object
 data(county.fips, package="maps")
 test_that("county level choropleths return ggplots at discrete and continuous lods", {
   df = data.frame(region=county.fips$fips, value=sample(100, nrow(county.fips), replace=T))
-  expect_that(class(choroplethr(df, lod="county", num_buckets=1)), matches(c("gg", "ggplot")))
-  expect_that(class(choroplethr(df, lod="county", num_buckets=9)), matches(c("gg", "ggplot")))
+  expect_equal(class(choroplethr(df, lod="county", num_buckets=1)), c("gg", "ggplot"))
+  expect_equal(class(choroplethr(df, lod="county", num_buckets=9)), c("gg", "ggplot"))
 })
 
-# do I need data(zipcode) here?
-# rendering a test zip-level map at the continuous and discrete scales needs to return a valid ggplot object
 data(zipcode, package="zipcode")
 test_that("zip level choropleths return ggplots at discrete and continuous lods", {  
   df = data.frame(region=zipcode$zip, value = sample(100, nrow(zipcode), replace=T))
-  expect_that(class(choroplethr(df, lod="zip", num_buckets=1)), matches(c("gg", "ggplot")))
-  expect_that(class(choroplethr(df, lod="zip", num_buckets=9)), matches(c("gg", "ggplot")))
+  expect_equal(class(choroplethr(df, lod="zip", num_buckets=1)), c("gg", "ggplot"))
+  expect_equal(class(choroplethr(df, lod="zip", num_buckets=9)), c("gg", "ggplot"))
 })
---
+++ inst/tests/test-choroplethr_acs.R
@@ -0,0 +1,16 @@
+context("choroplethr_acs")
+
+test_that("state level acs choropleth returns ggplots with discrete and continuous lods", {
+  expect_equal(class(choroplethr_acs("B00001", "state", num_buckets=1)), c("gg", "ggplot"))
+  expect_equal(class(choroplethr_acs("B00001", "state", num_buckets=9)), c("gg", "ggplot"))
+})
+                        
+test_that("county level acs choropleth returns ggplots with discrete and continuous lods", {
+  expect_equal(class(choroplethr_acs("B00001", "county", num_buckets=1)), c("gg", "ggplot"))
+  expect_equal(class(choroplethr_acs("B00001", "county", num_buckets=9)), c("gg", "ggplot"))
+})
+
+test_that("zip level acs choropleth returns ggplots with discrete and continuous lods", {
+  expect_equal(class(choroplethr_acs("B00001", "zip", num_buckets=1)), c("gg", "ggplot"))
+  expect_equal(class(choroplethr_acs("B00001", "zip", num_buckets=9)), c("gg", "ggplot"))
+})
