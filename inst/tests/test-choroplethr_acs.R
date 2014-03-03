34f974f63cadce9452a744ca9660bedfbca17490
alamstein-trulia <alamstein@trulia.com>
2014-03-03 12:31:13 -0800

    Update test-choroplethr_acs.R

--- inst/tests/test-choroplethr_acs.R
+++ inst/tests/test-choroplethr_acs.R
@@ -1,8 +1,5 @@
 context("choroplethr_acs")
 
-# these tests are valid but must be commented out for submissions to CRAN.
-# because they require a valid census api key installed with the acs package, which
-# CRAN does not have.
 test_that("state level acs choropleth returns ggplots with discrete and continuous lods", {
   expect_equal(class(choroplethr_acs("B00001", "state", num_buckets=1)), c("gg", "ggplot"))
   expect_equal(class(choroplethr_acs("B00001", "state", num_buckets=9)), c("gg", "ggplot"))
