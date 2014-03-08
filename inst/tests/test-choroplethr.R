2d7d970a3070711268ca5a3dda9da00627ea49bf
Ari Lamstein <arilamstein@gmail.com>
2014-03-08 14:06:50 -0800

    Use expect_is

--- inst/tests/test-choroplethr.R
+++ inst/tests/test-choroplethr.R
@@ -2,20 +2,20 @@ context("choroplethr")
 
 test_that("state level choropleths return ggplots with discrete and continuous lods", {
   df = data.frame(region=state.abb, value=sample(100, 50))
-  expect_equal(class(choroplethr(df, lod="state", num_buckets=1)), c("gg", "ggplot"))
-  expect_equal(class(choroplethr(df, lod="state", num_buckets=9)), c("gg", "ggplot"))
+  expect_is(choroplethr(df, lod="state", num_buckets=1), "ggplot")
+  expect_is(choroplethr(df, lod="state", num_buckets=9), "ggplot")
 })
                         
 data(county.fips, package="maps")
 test_that("county level choropleths return ggplots at discrete and continuous lods", {
   df = data.frame(region=county.fips$fips, value=sample(100, nrow(county.fips), replace=T))
-  expect_equal(class(choroplethr(df, lod="county", num_buckets=1)), c("gg", "ggplot"))
-  expect_equal(class(choroplethr(df, lod="county", num_buckets=9)), c("gg", "ggplot"))
+  expect_is(choroplethr(df, lod="county", num_buckets=1), "ggplot")
+  expect_is(choroplethr(df, lod="county", num_buckets=9), "ggplot")
 })
 
 data(zipcode, package="zipcode")
 test_that("zip level choropleths return ggplots at discrete and continuous lods", {  
   df = data.frame(region=zipcode$zip, value = sample(100, nrow(zipcode), replace=T))
-  expect_equal(class(choroplethr(df, lod="zip", num_buckets=1)), c("gg", "ggplot"))
-  expect_equal(class(choroplethr(df, lod="zip", num_buckets=9)), c("gg", "ggplot"))
+  expect_is(choroplethr(df, lod="zip", num_buckets=1), "ggplot")
+  expect_is(choroplethr(df, lod="zip", num_buckets=9), "ggplot")
 })
--- inst/tests/test-render_choropleth.R
+++ inst/tests/test-render_choropleth.R
@@ -4,7 +4,7 @@ test_that("state render_choropleth continuous returns a ggplot object", {
   df     = get_acs_df("B01003", "state") # population
   df.map = bind_df_to_map(df, "state")
   x      = render_choropleth(df.map, "state", "title", "scale name")
-  expect_equal(class(x), c("gg", "ggplot"))
+  expect_is(x, "ggplot")
 })
 
 test_that("state render_choropleth discrete returns a ggplot object", {
@@ -12,14 +12,14 @@ test_that("state render_choropleth discrete returns a ggplot object", {
   df.map = bind_df_to_map(df, "state")
   df.map$value = cut2(df.map$value, cuts=c(0,1000000,Inf))
   x      = render_choropleth(df.map, "state", "title", "scale name")
-  expect_equal(class(x), c("gg", "ggplot"))
+  expect_is(x, "ggplot")
 })
 
 test_that("county render_choropleth continuous returns a ggplot object", {
   df     = get_acs_df("B01003", "county") # population
   df.map = bind_df_to_map(df, "county")
   x      = render_choropleth(df.map, "county", "Counties with a population over 1M", "Population")
-  expect_equal(class(x), c("gg", "ggplot"))
+  expect_is(x, "ggplot")
 })
 
 test_that("county render_choropleth discrete returns a ggplot object", {
@@ -27,23 +27,20 @@ test_that("county render_choropleth discrete returns a ggplot object", {
   df.map = bind_df_to_map(df, "county")
   df.map$value = cut2(df.map$value, cuts=c(0,1000000,Inf))
   x      = render_choropleth(df.map, "county", "Counties with a population over 1M", "Population")
-  expect_equal(class(x), c("gg", "ggplot"))
+  expect_is(x, "ggplot")
 })
 
-# comment out these tests for now because - even though they are passing locally - they are causing 
-# travis ci to fail.  The reported error occurs in get_acs_df, which causes an NA coercion warning for 
-# this table with zips.  until I get a better work around, just comment it out.
-#test_that("zip render_choropleth continuous returns a ggplot object", {
-#  df       = get_acs_df("B01003", "zip") # population
-#  df.map   = bind_df_to_map(df, "zip")
-#  x        = render_choropleth(df.map, "zip")
-#  expect_equal(class(x), c("gg", "ggplot"))
-#})
+test_that("zip render_choropleth continuous returns a ggplot object", {
+  df       = get_acs_df("B01003", "zip") # population
+  df.map   = bind_df_to_map(df, "zip")
+  x        = render_choropleth(df.map, "zip")
+  expect_is(x, "ggplot")
+})
 
-#test_that("zip render_choropleth discrete returns a ggplot object", {
-#  df       = get_acs_df("B01003", "zip") # population
-#  df.map   = bind_df_to_map(df, "zip")
-#  df$value = cut2(df$value, g=3) # 3 equally-sized groups
-#  x = render_choropleth(df.map, "zip")
-#  expect_equal(class(x), c("gg", "ggplot"))
-#})
+test_that("zip render_choropleth discrete returns a ggplot object", {
+  df       = get_acs_df("B01003", "zip") # population
+  df.map   = bind_df_to_map(df, "zip")
+  df$value = cut2(df$value, g=3) # 3 equally-sized groups
+  x = render_choropleth(df.map, "zip")
+  expect_is(x, "ggplot")
+})
