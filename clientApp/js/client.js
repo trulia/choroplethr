6e59ee7b143b4ae0f1a12660b1e0e26026aaec9a
Brian Johnson <bjohnson@trulia.com>
2014-03-19 12:10:35 -0700

    Adding javascript client for browsing historical maps

---
+++ clientApp/js/client.js
@@ -0,0 +1,21 @@
+angular.module('AnimatedMaps', ['ngAnimate'])
+  .controller('ClientApp', ['$scope', function($scope) {
+    'use strict';
+    $scope.dateValue = 0;
+    $scope.intervalRef = null;
+    $scope.maxMaps = 100;
+    $scope.minMaps = 0;
+    $scope.play = function() {
+      $scope.intervalRef = setInterval($scope.nextMap, 1000);
+    };
+    $scope.stop = function() {
+      clearInterval($scope.intervalRef);
+    };
+    $scope.nextMap = function() {
+      $scope.$apply(function() {
+        $scope.dateValue = Number($scope.dateValue) + 1;
+      });
+    };
+  }]).run(function($rootScope) {
+    'use strict';
+  });
