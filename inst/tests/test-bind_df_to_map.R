2ec68459174cef04d70cf61daf7d28d689b73cc5
Ari Lamstein <alamstein@trulia.com>
2014-05-04 10:28:26 -0700

    fix tests

--- inst/tests/test-bind_df_to_map.R
+++ inst/tests/test-bind_df_to_map.R
@@ -4,7 +4,7 @@ test_that("state df gets bound to map",
 {
   df = data.frame(region=state.abb, value=sample(100, 50))
   df.map = bind_df_to_map(df, "state")
-  expect_equal(15527, nrow(df.map))
+  expect_equal(15537, nrow(df.map))
 })
 
 test_that("county df gets bound to map", {
@@ -17,6 +17,6 @@ test_that("county df gets bound to map", {
 test_that("zip df gets bound to map", {
   data(zipcode, package="zipcode", envir=environment())
   df = data.frame(region=zipcode$zip, value = sample(100, nrow(zipcode), replace=TRUE))
-  df.map = bind_df_to_map(df, "county")
-  expect_equal(44548, nrow(df.map))
+  df.map = bind_df_to_map(df, "zip")
+  expect_equal(42714, nrow(df.map))
 })
