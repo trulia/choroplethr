e2c0bb1a16efc8f4364b3072a49c0edab80ada07
Ari Lamstein <alamstein@trulia.com>
2014-05-11 10:17:37 -0700

    the basic bookkeeping for a new version

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,5 +1,5 @@
 library(testthat)
 library(choroplethr)
 
-#disable tests for submission to cran, because my tests require a key to the us census api
-#test_package("choroplethr")
+#disable tests when submitting to cran, because my tests require a key to the us census api
+test_package("choroplethr")
