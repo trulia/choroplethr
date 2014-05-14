428c4a367ae40c4bbc4cc2bcf74c4e3e44918e9b
Ari Lamstein <alamstein@trulia.com>
2014-05-14 16:23:55 -0700

    export theme_clean. add useful examples to all.us.states.map.df

--- R/common.R
+++ R/common.R
@@ -46,23 +46,27 @@ subset_map = function(lod, states)
   df[df$region %in% states, ]
 }
 
+#' Create a simple ggplot2 theme for rendering choropleths
+#' 
+#' Removes axes, margins and sets the background to white.
+#' 
 #' @importFrom grid unit
-# this code, with minor modifications comes from section 13.19 
+#' @export
+#' @references This code, with minor modifications comes from section 13.19 
 # "Making a Map with a Clean Background" of "R Graphics Cookbook" by Winston Chang.  
-# Reused with permission. Create a theme with many of the background elements removed
-theme_clean = function(base_size = 12)
+# Reused with permission. 
+theme_clean = function()
 {
-  theme_grey(base_size);
   theme(
     axis.title        = element_blank(),
     axis.text         = element_blank(),
-#     panel.background  = element_blank(),
+    panel.background  = element_blank(),
     panel.grid        = element_blank(),
     axis.ticks.length = unit(0, "cm"),
     axis.ticks.margin = unit(0, "cm"),
     panel.margin      = unit(0, "lines"),
     plot.margin       = unit(c(0, 0, 0, 0), "lines"),
-    complete = TRUE
+    complete          = TRUE
   )
 }
 
