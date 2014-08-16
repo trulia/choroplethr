b467af16a242df2fc4e450f22893ab1ae6e9da7f
Ari Lamstein <alamstein@trulia.com>
2014-08-16 15:12:10 -0700

    file needs to be commented out in order for package to build

--- R/state_choropleth_test.R
+++ R/state_choropleth_test.R
@@ -1,7 +1,7 @@
-data(choroplethr)
-s = StateChoropleth$new(df_pop_state)
-s$render()
+#data(choroplethr)
+#s = StateChoropleth$new(df_pop_state)
+#s$render()
 
-s
-s$prepare_map()
-head(s$choropleth.df)
+#s
+#s$prepare_map()
+#head(s$choropleth.df)
