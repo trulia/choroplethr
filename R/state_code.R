411742b1e280e0e8ac160eeb438916dccaa01d89
Ari Lamstein <alamstein@trulia.com>
2014-08-24 10:18:36 -0700

    simple state choropleth function

--- R/state_code.R
+++ R/state_code.R
@@ -1,3 +1,51 @@
+#' Simple US State map 
+#' 
+#' AK and HI not rendered as insets
+#' @export
+#' 
+SimpleStateChoropleth = R6Class("SimpleStateChoropleth",
+  inherit = Choropleth,
+  public = list(
+  
+  # initialize with a US State Map
+  initialize = function(user.df)
+  {
+    data(state.map)
+    super$initialize(state.map, user.df)
+  }
+  )
+)
+
+#' Create a simple choropleth of USA States
+#' 
+#' AK and HI not rendered as insets
+#' 
+#' @param df A data.frame with a column named "region" and a column named "value".  
+#' @param title An optional title for the map.  
+#' @param legend_name An optional name for the legend.  
+#' @param num_buckets The number of equally sized buckets to places the values in.  A value of 1 
+#' will use a continuous scale, and a value in [2, 9] will use that many buckets. 
+#' 
+#' @examples
+#' data(df_pop_state)
+#' simple_state_choropleth(df_pop_state, title="US 2012 Population Estimates", legend_name="Population")
+#'
+#' @export
+#' @importFrom Hmisc cut2
+#' @importFrom stringr str_extract_all
+#' @importFrom ggplot2 ggplot aes geom_polygon scale_fill_brewer ggtitle theme theme_grey element_blank geom_text
+#' @importFrom ggplot2 scale_fill_continuous scale_colour_brewer
+#' @importFrom scales comma
+#' @importFrom grid unit
+simple_state_choropleth = function(df, title="", legend_name="", num_buckets=7)
+{
+  c = SimpleStateChoropleth$new(df)
+  c$title       = title
+  c$legend_name = legend_name
+  
+  c$render(num_buckets)
+}
+
 #' Create a state-level choropleth
 #' @export
 #' @importFrom dplyr left_join
