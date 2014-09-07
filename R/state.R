d8aa5445e238907823588766761ae25bd5791087
Ari Lamstein <alamstein@trulia.com>
2014-09-06 21:54:59 -0700

    subsetting for state map

--- R/state.R
+++ R/state.R
@@ -24,6 +24,9 @@ StateChoropleth = R6Class("StateChoropleth",
 #' @param legend_name An optional name for the legend.  
 #' @param num_buckets The number of equally sized buckets to places the values in.  A value of 1 
 #' will use a continuous scale, and a value in [2, 9] will use that many buckets. 
+#' @param states Which states of the map to show. If NULL, show all. 
+#' Type "data(state.names); ?state.names" to see the list of states. States must 
+#' correspond to the "name" column of state.names.
 #' 
 #' @examples
 #' data(df_pop_state)
@@ -36,11 +39,12 @@ StateChoropleth = R6Class("StateChoropleth",
 #' @importFrom ggplot2 scale_fill_continuous scale_colour_brewer
 #' @importFrom scales comma
 #' @importFrom grid unit
-state_choropleth = function(df, title="", legend_name="", num_buckets=7)
+state_choropleth = function(df, title="", legend_name="", num_buckets=7, states=NULL)
 {
   c = StateChoropleth$new(df)
   c$title       = title
   c$legend_name = legend_name
+  c$regions     = states
   
   c$render(num_buckets)
 }
