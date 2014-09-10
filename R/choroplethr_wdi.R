3aa58f249700f4c475e4f5c197b7754000ed332c
Ari Lamstein <alamstein@trulia.com>
2014-09-10 12:38:11 -0700

    examples for choroplethr_wdi

--- R/choroplethr_wdi.R
+++ R/choroplethr_wdi.R
@@ -8,16 +8,29 @@
 #' will use a continuous scale, and a value in [2, 9] will use that many buckets.  For
 #' example, 2 will show values above or below the median, and 9 will show the maximum
 #' resolution.  Defaults to 9.
-
+#'
+#' @examples
+#' \dontrun{
+#' # See http://data.worldbank.org/indicator/SP.POP.TOTL
+#' choroplethr_wdi(code="SP.POP.TOTL", year=2012, title="2012 Per Capita Income", num_buckets=1)
+#'
+#' # See http://data.worldbank.org/indicator/SP.DYN.LE00.IN 
+#' choroplethr_wdi(code="SP.DYN.LE00.IN", year=2012, title="2012 Life Expectancy Estimates")
+#'
+#' # See http://data.worldbank.org/indicator/NY.GDP.PCAP.CD 
+#' choroplethr_wdi(code="NY.GDP.PCAP.CD", year=2012, title="2012 Per Capita Income") 
+#' }
+#' 
 #' @return A choropleth.
 #' 
+#' @references Uses the WDI function from the WDI package by Vincent Arel-Bundock.
 #' @export
 #' @importFrom WDI WDI
-choroplethr_wdi = function(code="SP.POP.TOTL", year=2012, title=NULL, countries=NULL, num_buckets=9)
+choroplethr_wdi = function(code="SP.POP.TOTL", year=2012, title=NULL, countries=NULL, num_buckets=7)
 {
   if (is.null(title))
   {
-    title = paste0("WDI indicator ", code, " for year ", year)    
+    title = paste0("WDI Indicator ", code, " for year ", year)    
   }
   
   data(country.names, package="choroplethr", envir=environment())
