4beb7adf4eab5fb6c89c4445efc33b8064e0902b
Ari Lamstein <alamstein@trulia.com>
2014-09-10 14:15:33 -0700

    choroplethr_wdi polish

--- R/choroplethr_wdi.R
+++ R/choroplethr_wdi.R
@@ -3,11 +3,11 @@
 #' @param code The WDI code to use.
 #' @param year The year of data to use.
 #' @param title A title for the map.  If not specified, automatically generated to include WDI code and year.
-#' @param countries If null, render all countries. Otherwise must be a vector of country names.  See ?country.names for a list of valid country names.
 #' @param num_buckets The number of equally sized buckets to places the values in.  A value of 1 
 #' will use a continuous scale, and a value in [2, 9] will use that many buckets.  For
 #' example, 2 will show values above or below the median, and 9 will show the maximum
 #' resolution.  Defaults to 9.
+#' @param countries If null, render all countries. Otherwise must be a vector of country names.  See ?country.names for a list of valid country names.
 #'
 #' @examples
 #' \dontrun{
@@ -26,7 +26,7 @@
 #' @references Uses the WDI function from the WDI package by Vincent Arel-Bundock.
 #' @export
 #' @importFrom WDI WDI
-choroplethr_wdi = function(code="SP.POP.TOTL", year=2012, title=NULL, countries=NULL, num_buckets=7)
+choroplethr_wdi = function(code="SP.POP.TOTL", year=2012, title="", num_buckets=7, countries=NULL)
 {
   if (is.null(title))
   {
@@ -46,5 +46,5 @@ choroplethr_wdi = function(code="SP.POP.TOTL", year=2012, title=NULL, countries=
   data = merge(data, country.names)
   data$value = data[, names(data) == code] # choroplethr requires value column to be named "valued"
   
-  choroplethr(data, "world", title=title, countries=countries, num_buckets=num_buckets)
+  country_choropleth(data, title, "", num_buckets, countries=countries)
 }
