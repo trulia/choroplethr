ea6895a55a01aa36b376037808cc71db5f2e2d46
Ari Lamstein <alamstein@trulia.com>
2014-09-18 21:19:54 -0700

    fix animation example

--- R/animate.R
+++ R/animate.R
@@ -19,14 +19,14 @@
 #' \dontrun{
 #' data(df_president_ts)
 #' ?df_president_ts # time series of all US presidential elections 1789-2012
-#' 
+#'
 #' # create a list of choropleths of presidential election results for each year
 #' choropleths = list()
-#' for (i in 2:ncol(df_president_ts)) {
-#'   df           = df_president_ts[, c(1, i)]
+#' for (i in 1:(ncol(df_president_ts)-1)) {
+#'   df           = df_president_ts[, c(ncol(df_president_ts), i)]
 #'   colnames(df) = c("region", "value")
 #'   title        = paste0("Presidential Election Results: ", colnames(df_president_ts)[i])
-#'   choropleths[[i-1]] = state_choropleth(df, title=title) 
+#'  choropleths[[i]] = state_choropleth(df, title=title)
 #' }
 #'
 #' # set working directory and animate
