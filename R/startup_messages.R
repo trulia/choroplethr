02ff4565acf4b58ad9384d110285dbca64e16b2f
Ari Lamstein <arilamstein@gmail.com>
2016-03-22 16:22:08 -0700

    change startup messages

--- R/startup_messages.R
+++ R/startup_messages.R
@@ -2,12 +2,11 @@
   if (!interactive() || stats::runif(1) > 0.1) return()
   
   tips <- c(
-    "Need help? Try the Choroplethr Google Group: https://groups.google.com/forum/#!forum/choroplethr.",
-    "Find out what's changed in Choroplethr: https://github.com/arilamstein/choroplethr/blob/master/NEWS.",
-    "Use suppressPackageStartupMessages() to eliminate package startup messages.",
-    "Stackoverflow is a great place to get help: http://stackoverflow.com/tags/r.",
+    "Stay up to date on choroplethr by following the blog: http://arilamstein.com/blog",
     "Need help getting started? Try the free course 'Learn to Map Census Data in R': http://arilamstein.com/free-course/",
-    "Want to take an in depth course on Choroplethr? Try 'Mapmaking in R with Choroplethr': http://courses.arilamstein.com/courses/mapmaking-r-choroplethr"
+    "Technical question? Try the Choroplethr Google Group: https://groups.google.com/forum/#!forum/choroplethr.",
+    "Want to take an in depth course on Choroplethr? Try 'Mapmaking in R with Choroplethr': http://courses.arilamstein.com/courses/mapmaking-r-choroplethr",
+    "Use suppressPackageStartupMessages() to eliminate package startup messages."
   )
   
   tip <- sample(tips, 1)
