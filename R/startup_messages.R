f96d962a1fc2c0aa89fddf31f988dab524ad6d86
Ari Lamstein <arilamstein@gmail.com>
2016-03-30 09:46:39 -0700

    Move acs package from import to depends
    Move "acs" package from Import to Depends. While I don't think that
    this should be
    necessary, it seems to be the only way to fix a bug I describe here:
    https://stat.ethz.ch/pipermail/r-package-devel/2016q1/000760.html

--- R/startup_messages.R
+++ R/startup_messages.R
@@ -4,7 +4,7 @@
   tips <- c(
     "Stay up to date on choroplethr by following the blog: http://arilamstein.com/blog",
     "Need help getting started? Try the free course 'Learn to Map Census Data in R': http://arilamstein.com/free-course/",
-    "Technical question? Try the Choroplethr Google Group: https://groups.google.com/forum/#!forum/choroplethr.",
+    "Technical question? Try stack overflow: http://stackoverflow.com/questions/tagged/r",
     "Want to take an in depth course on Choroplethr? Try 'Mapmaking in R with Choroplethr': http://courses.arilamstein.com/courses/mapmaking-r-choroplethr",
     "Use suppressPackageStartupMessages() to eliminate package startup messages."
   )
