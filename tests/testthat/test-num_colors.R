5f4efdc242785ef8abf4fd4a576089765dc84507
Ari Lamstein <arilamstein@gmail.com>
2017-04-15 10:59:35 -0700

    update and move tests

--- tests/testthat/test-num_colors.R
+++ tests/testthat/test-num_colors.R
@@ -1,10 +1,5 @@
 context("test num_colors")
 
-test_that("state num_colors = 0 throws exception", {
-  data(df_pop_state, package="choroplethr")
-  expect_error(state_choropleth(df_pop_state, num_colors=0))
-})
-
 test_that("state num_colors = 10 throws exception", {
   data(df_pop_state, package="choroplethr")
   expect_error(state_choropleth(df_pop_state, num_colors=10))
@@ -18,6 +13,11 @@ test_that("county float num_colors throws exception", {
 test_that("zip string num_colors throws exception", {
   data(df_pop_state, package="choroplethr")
   expect_error(state_choropleth(df_pop_state, num_colors="hello"))
+})
+
+test_that("state num_colors = 0 returns ggplot", {
+  data(df_pop_state, package="choroplethr")
+  expect_is(state_choropleth(df_pop_state, num_colors=0), "ggplot")
 })
 
 test_that("country num_colors=1 returns ggplot", {
